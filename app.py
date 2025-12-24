87c19c216122c6bb152d24358c422be3d2ef3b46
t-kramer <t.kramer@berkeley.edu>
2025-12-24 11:35:10 -0800

    build: prep for deployment

--- app.py
+++ app.py
@@ -48,17 +48,17 @@ def log_session_id(session_data):
     return session_data
 
 
-# if __name__ == "__main__":
-#     app.run(
-#         debug=False,
-#         host="0.0.0.0",
-#         port=8080,
-#     )
-
-
 if __name__ == "__main__":
     app.run(
-        debug=True,
-        host="localhost",
-        port=8050,
+        debug=False,
+        host="0.0.0.0",
+        port=8080,
     )
+
+
+# if __name__ == "__main__":
+#     app.run(
+#         debug=True,
+#         host="localhost",
+#         port=8050,
+#     )
